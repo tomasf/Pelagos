bbabb8e998b52b4d7e282f987da3b13014007dde
Tomas Franzén <tomas@tomasf.se>
2026-01-25 00:34:48 +0100

    Use Command enum for path parsing

--- Sources/Pelagos/Path/PathParser.swift
+++ Sources/Pelagos/Path/PathParser.swift
@@ -8,19 +8,23 @@ struct PathParser {
         var scanner = PathScanner(data)
         var segments: [PathSegment] = []
 
-        while let command = scanner.scanCommand() {
-            let newSegments = try parseCommand(command, scanner: &scanner)
-            segments.append(contentsOf: newSegments)
+        while true {
+            if let command = try scanner.scanCommand() {
+                let newSegments = try parseCommand(command, scanner: &scanner)
+                segments.append(contentsOf: newSegments)
+            } else {
+                break
+            }
         }
 
         return segments
     }
 
-    private func parseCommand(_ command: Character, scanner: inout PathScanner) throws -> [PathSegment] {
+    private func parseCommand(_ command: Command, scanner: inout PathScanner) throws -> [PathSegment] {
         var segments: [PathSegment] = []
 
         switch command {
-        case "M":
+        case .moveTo:
             guard let point = scanner.scanPoint() else { throw PathParseError.missingArguments }
             segments.append(.moveTo(point))
             // Subsequent points are implicit lineTo
@@ -28,56 +32,56 @@ struct PathParser {
                 segments.append(.lineTo(point))
             }
 
-        case "m":
+        case .moveToRelative:
             guard let point = scanner.scanPoint() else { throw PathParseError.missingArguments }
             segments.append(.moveToRelative(point))
             while let point = scanner.scanPoint() {
                 segments.append(.lineToRelative(point))
             }
 
-        case "L":
+        case .lineTo:
             guard let point = scanner.scanPoint() else { throw PathParseError.missingArguments }
             segments.append(.lineTo(point))
             while let point = scanner.scanPoint() {
                 segments.append(.lineTo(point))
             }
 
-        case "l":
+        case .lineToRelative:
             guard let point = scanner.scanPoint() else { throw PathParseError.missingArguments }
             segments.append(.lineToRelative(point))
             while let point = scanner.scanPoint() {
                 segments.append(.lineToRelative(point))
             }
 
-        case "H":
+        case .horizontalLineTo:
             guard let x = scanner.scanNumber() else { throw PathParseError.missingArguments }
             segments.append(.horizontalLineTo(x))
             while let x = scanner.scanNumber() {
                 segments.append(.horizontalLineTo(x))
             }
 
-        case "h":
+        case .horizontalLineToRelative:
             guard let x = scanner.scanNumber() else { throw PathParseError.missingArguments }
             segments.append(.horizontalLineToRelative(x))
             while let x = scanner.scanNumber() {
                 segments.append(.horizontalLineToRelative(x))
             }
 
-        case "V":
+        case .verticalLineTo:
             guard let y = scanner.scanNumber() else { throw PathParseError.missingArguments }
             segments.append(.verticalLineTo(y))
             while let y = scanner.scanNumber() {
                 segments.append(.verticalLineTo(y))
             }
 
-        case "v":
+        case .verticalLineToRelative:
             guard let y = scanner.scanNumber() else { throw PathParseError.missingArguments }
             segments.append(.verticalLineToRelative(y))
             while let y = scanner.scanNumber() {
                 segments.append(.verticalLineToRelative(y))
             }
 
-        case "C":
+        case .curveTo:
             while let c1 = scanner.scanPoint(),
                   let c2 = scanner.scanPoint(),
                   let end = scanner.scanPoint() {
@@ -85,7 +89,7 @@ struct PathParser {
             }
             if segments.isEmpty { throw PathParseError.missingArguments }
 
-        case "c":
+        case .curveToRelative:
             while let c1 = scanner.scanPoint(),
                   let c2 = scanner.scanPoint(),
                   let end = scanner.scanPoint() {
@@ -93,45 +97,45 @@ struct PathParser {
             }
             if segments.isEmpty { throw PathParseError.missingArguments }
 
-        case "S":
+        case .smoothCurveTo:
             while let c2 = scanner.scanPoint(), let end = scanner.scanPoint() {
                 segments.append(.smoothCurveTo(control2: c2, end: end))
             }
             if segments.isEmpty { throw PathParseError.missingArguments }
 
-        case "s":
+        case .smoothCurveToRelative:
             while let c2 = scanner.scanPoint(), let end = scanner.scanPoint() {
                 segments.append(.smoothCurveToRelative(control2: c2, end: end))
             }
             if segments.isEmpty { throw PathParseError.missingArguments }
 
-        case "Q":
+        case .quadraticCurveTo:
             while let control = scanner.scanPoint(), let end = scanner.scanPoint() {
                 segments.append(.quadraticCurveTo(control: control, end: end))
             }
             if segments.isEmpty { throw PathParseError.missingArguments }
 
-        case "q":
+        case .quadraticCurveToRelative:
             while let control = scanner.scanPoint(), let end = scanner.scanPoint() {
                 segments.append(.quadraticCurveToRelative(control: control, end: end))
             }
             if segments.isEmpty { throw PathParseError.missingArguments }
 
-        case "T":
+        case .smoothQuadraticCurveTo:
             guard let point = scanner.scanPoint() else { throw PathParseError.missingArguments }
             segments.append(.smoothQuadraticCurveTo(point))
             while let point = scanner.scanPoint() {
                 segments.append(.smoothQuadraticCurveTo(point))
             }
 
-        case "t":
+        case .smoothQuadraticCurveToRelative:
             guard let point = scanner.scanPoint() else { throw PathParseError.missingArguments }
             segments.append(.smoothQuadraticCurveToRelative(point))
             while let point = scanner.scanPoint() {
                 segments.append(.smoothQuadraticCurveToRelative(point))
             }
 
-        case "A":
+        case .arcTo:
             while let arc = scanner.scanArcArguments() {
                 segments.append(.arcTo(
                     rx: arc.rx,
@@ -144,7 +148,7 @@ struct PathParser {
             }
             if segments.isEmpty { throw PathParseError.missingArguments }
 
-        case "a":
+        case .arcToRelative:
             while let arc = scanner.scanArcArguments() {
                 segments.append(.arcToRelative(
                     rx: arc.rx,
@@ -157,15 +161,35 @@ struct PathParser {
             }
             if segments.isEmpty { throw PathParseError.missingArguments }
 
-        case "Z", "z":
+        case .closePath, .closePathRelative:
             segments.append(.closePath)
-
-        default:
-            throw PathParseError.unknownCommand(command)
         }
 
         return segments
     }
+}
+
+enum Command: String {
+    case moveTo = "M"
+    case moveToRelative = "m"
+    case lineTo = "L"
+    case lineToRelative = "l"
+    case horizontalLineTo = "H"
+    case horizontalLineToRelative = "h"
+    case verticalLineTo = "V"
+    case verticalLineToRelative = "v"
+    case curveTo = "C"
+    case curveToRelative = "c"
+    case smoothCurveTo = "S"
+    case smoothCurveToRelative = "s"
+    case quadraticCurveTo = "Q"
+    case quadraticCurveToRelative = "q"
+    case smoothQuadraticCurveTo = "T"
+    case smoothQuadraticCurveToRelative = "t"
+    case arcTo = "A"
+    case arcToRelative = "a"
+    case closePath = "Z"
+    case closePathRelative = "z"
 }
 
 enum PathParseError: Error, Equatable {
@@ -198,11 +222,14 @@ struct PathScanner {
         index = string.index(after: index)
     }
 
-    mutating func scanCommand() -> Character? {
+    mutating func scanCommand() throws -> Command? {
         skipWhitespaceAndCommas()
         guard let c = peek(), c.isLetter else { return nil }
         advance()
-        return c
+        guard let command = Command(rawValue: String(c)) else {
+            throw PathParseError.unknownCommand(c)
+        }
+        return command
     }
 
     mutating func scanNumber() -> Double? {
