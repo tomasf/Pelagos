b28b453a04adf9db749a6f2123905082a6a87962
Tomas Franzén <tomas@tomasf.se>
2026-01-25 21:56:00 +0100

    Set minimum platform requirements to match Nodal dependency

--- Package.swift
+++ Package.swift
@@ -4,13 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Pelagos",
-    platforms: [
-        .macOS(.v15),
-        .iOS(.v18),
-        .tvOS(.v18),
-        .watchOS(.v11),
-        .visionOS(.v2)
-    ],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(name: "Pelagos", targets: ["Pelagos"]),
         .executable(name: "PelagosCLI", targets: ["PelagosCLI"])
